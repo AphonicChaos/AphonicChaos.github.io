0c1928fe2db5f9c11ef9d80710030d01514e1c8c
Edwin <emarshall85@gmail.com>
2025-03-14 02:13:30 -0700

    Templates are valid mustache, but no library parses that

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Aeson
 import Data.Text
-import Text.Megaparsec
+-- import Text.Megaparsec
 import Text.Mustache
-import Data.Text.Lazy.IO qualified as TL
+import Data.Text.IO qualified as T
+
+data PostContext = PostContext 
+  { postTitle :: Text
+  , teaser :: Text
+  , date :: Text
+  , url :: Text
+  }
+  deriving (Eq, Show)
+
+instance ToMustache PostContext where
+  toMustache ctx =
+    object [ "title" ~> postTitle ctx 
+           , "teaser" ~> teaser ctx 
+           , "date" ~> date ctx
+           , "url" ~> url ctx
+           ]
+
+data PageContext = PageContext
+  { pageTitle :: Text
+  , onHome :: Bool
+  , onPortfolio :: Bool
+  , onArchive :: Bool
+  , posts :: [PostContext]
+  } 
+  deriving (Eq, Show)
+
+instance ToMustache PageContext where
+  toMustache ctx = 
+   object  [ "title" ~> pageTitle ctx
+           , "onHome" ~> onHome ctx
+           , "onPortfolio " ~> onPortfolio ctx
+           , "onArchive" ~> onArchive ctx
+           ]
 
 main :: IO ()
 main = do
-  let res = compileMustacheText "foo"
-              """
-              Hi, {{name}}! You have:
-              {{#things}}
-              * {{.}}
-              {{/things}}
-              """
-  case res of
-    Left bundle -> putStrLn $ errorBundlePretty bundle
-    Right template -> TL.putStr . renderMustache template $
-      object [ "name" .= ("John" :: Text)
-             , "things" .= ["pen" :: Text, "chandle", "egg"]
-             ]
-
-
+  let ctx = PageContext { pageTitle = "Aphonic"
+                        , onHome = False
+                        , onPortfolio = True
+                        , onArchive = False
+                        , posts = []
+                        }
+  indexE <- automaticCompile ["site/templates"] "index.mustache"
+  case indexE of 
+    Left err -> print err
+    Right indexT -> do 
+      print indexT
+      T.putStr $ substitute indexT ctx
---
+++ old/site.hs
@@ -0,0 +1,80 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/**" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    matchMetadata "posts/*" (not . isDraft) $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx = listField "posts" (teaserField "teaser" "content" <> postCtx) (return posts)
+                        <> constField "is-home" ""
+                        <> defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match (fromList ["portfolio.html"]) $ do
+      route $ idRoute
+      compile $ do
+          let portfolioCtx = constField "is-portfolio" "" 
+                          <> defaultContext
+                  
+          getResourceBody
+              >>= loadAndApplyTemplate "templates/default.html" portfolioCtx
+              >>= relativizeUrls
+
+    match (fromList ["resume.html"]) $ do
+      route $ idRoute
+      compile copyFileCompiler
+
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx = listField "posts" (teaserField "teaser" "content" <> postCtx) (return posts) 
+                          <> constField "title" "Archive"             
+                          <> constField "is-archive" ""
+                          <> defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+isDraft :: Metadata -> Bool
+isDraft = maybe False (== "true") . lookupString "draft"
